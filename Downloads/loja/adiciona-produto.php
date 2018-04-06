189f60b14fdf22c582fa5deedde5071966b26ce1
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-04-06 18:33:43 -0300

    Inclusão do arquivo conecta.php -faz a conexão da página com o banco de dados- e produto-lista.php-lista os dados do banco. Alterações em alguns arquivos antes criados

--- Downloads/loja/adiciona-produto.php
+++ Downloads/loja/adiciona-produto.php
@@ -1,25 +1,33 @@
 <?php include("cabecalho.php"); ?>
+<?php include("conecta.php");
 
-<?php
+function insereProduto($conexao, $nome, $preco) {
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    $resultadoDaInsercao = mysqli_query($conexao, $query);
+    return $resultadoDaInsercao;
+}
 
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
-$conexao = mysqli_connect('localhost', 'root', '', 'Produtos');
+
 $query = "insert into produtos(nome,preco) values ('{$nome}', {$preco})";
-if (mysqli_query($conexao, $query)) { ?>
-  <p class="alert-success">
+if (insereProduto($conexao, $nome, $preco)) { ?>
+  <p class="text-success">
       Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
   </p>
 
  <?php
-    } else {  ?>
-   <p class="alert-danger">
-       Produto <?= $nome ?>  não adicionado!
+    } else {
+        $msg = mysqli_error($conexao);
+       ?>
+
+   <p class="text-danger">
+       Produto <?= $nome ?>  não adicionado! <?= $msg?>
    </p>
 <?php
+
 }
 
-mysqli_close($conexao);
  ?>
 
 <?php include("rodape.php"); ?>
