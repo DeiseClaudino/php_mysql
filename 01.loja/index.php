3db71360c4078e72c34bf8ddada373a4ec251054
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-04-17 17:06:04 -0300

    Adicionando formulário de login no index.php

--- 01.loja/index.php
+++ 01.loja/index.php
@@ -1,3 +1,19 @@
 <?php include("cabecalho.php"); ?>
             <h1>Bem vindo!</h1>
+            <h2>Login</h2>
+<form action="login.php" method="post">
+  <table class="table">
+    <tr>
+      <td>Email</td>
+      <td><input type="email" name="email" class="form-control"></td>
+    </tr>
+    <tr>
+      <td>Senha</td>
+      <td><input type="password" name="senha" class="form-control"></td>
+    </tr>
+    <tr>
+      <button class="btn btn-primary">Login</button>
+    </tr>
+  </table>
+</form>
 <?php include("rodape.php"); ?>
