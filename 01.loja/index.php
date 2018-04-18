995e5c10557e6f969312e683922c12f170427f09
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-04-18 15:28:25 -0300

    Criando cookie para o usuário

--- 01.loja/index.php
+++ 01.loja/index.php
@@ -6,20 +6,31 @@
   <p class="alert-danger">Usuário ou senha Inválida!</p>
 <?php } ?>
             <h1>Bem vindo!</h1>
-            <h2>Login</h2>
-<form action="login.php" method="post">
-  <table class="table">
-    <tr>
+<?php
+if(isset($_COOKIE["usuario_logado"])) {
+?>
+  <p class="text-success">Você está logado como <?= $_COOKIE["usuario_logado"] ?></p>
+  <?php
+  } else {
+  ?>
+    <h2>Login</h2>
+  <form action="login.php" method="post">
+    <table class="table">
+      <tr>
       <td>Email</td>
       <td><input type="email" name="email" class="form-control"></td>
-    </tr>
-    <tr>
+      </tr>
+      <tr>
       <td>Senha</td>
       <td><input type="password" name="senha" class="form-control"></td>
-    </tr>
-    <tr>
+      </tr>
+      <tr>
       <td><button class="btn btn-primary">Login</button></td>
-    </tr>
-  </table>
-</form>
+      </tr>
+    </table>
+  </form>
+  <?php
+  }
+?>
+
 <?php include("rodape.php"); ?>
