01559ea229a6a57ba563d9ef6135f57788d2b3a0
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-04-25 13:33:21 -0300

    Alterando produto-formulario para que utilize as classes Categoria e Produto.

--- 01.loja/produto-formulario.php
+++ 01.loja/produto-formulario.php
@@ -1,21 +1,28 @@
 <?php require_once("cabecalho.php");
-require_once("banco-categoria.php");
-require_once("logica-usuario.php");
-verificaUsuario();
+  require_once("banco-categoria.php");
+  require_once("logica-usuario.php");
+  require_once("class/Produto.php");
+  require_once("class/Categoria.php");
+  verificaUsuario();
 
   $categorias = listaCategorias($conexao);
-  $produto = array("nome" => "", "descricao" => "", "preco"  => "", "categoria_id"  => "1");
+  $categoria = new Categoria();
+  $ccategoria->id = 1;
+
+  $produto = new Produto();
+  $produto->categoria = $categoria;
+
   $usado = "";
-?>
+  ?>
 
-<h1>Formulário de cadastro</h1>
-<form action="adiciona-produto.php" method="post">
-  <table class="table">
-      <?php include("produto-formulario-base.php"); ?>
-      <tr><td><button class="btn btn-primary" type="submit">Cadastrar</button></td></tr>
+  <h1>Formulário de cadastro</h1>
+  <form action="adiciona-produto.php" method="post">
+    <table class="table">
+        <?php include("produto-formulario-base.php"); ?>
+        <tr><td><button class="btn btn-primary" type="submit">Cadastrar</button></td></tr>
 
 
-</table>
-</form>
+    </table>
+  </form>
 
 <?php include("rodape.php"); #Fromulário para insercao de dados   ?>
