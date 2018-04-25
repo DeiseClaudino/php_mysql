8cf75fce0da728072e9f7aa20ad200324b6c6f8c
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-04-25 18:25:42 -0300

    Criando os getters and setters para as propriedades da classe

--- 01.loja/class/Produto.php
+++ 01.loja/class/Produto.php
@@ -15,6 +15,23 @@ class Produto{
       return $this->preco;
   }
 
+  public function getId() {
+    return $this->id;
+  }
+
+  public function setId($id) {
+      $this->id = $id;
+  }
+
+  public function getNome() {
+    return $this->nome;
+}
+
+public function setNome($nome) {
+    $this->nome = $nome;
+}
+
+
   public function getPreco() {
       return $this->private
   }
@@ -23,5 +40,48 @@ class Produto{
           $this->preco = $preco;
       }
   }
+
+  public function getdescricao() {
+    return $this->descricao;
+}
+
+public function setDescricao($descricao) {
+    $this->descricao = $descricao;
+}
+
+public function geCategoria() {
+  return $this->categoria;
+}
+
+public function setCategoria($categoria) {
+  $this->categoria = $categoria;
+}
+
+public function getUsado() {
+  return $this->usado;
+}
+
+public function setUsado($usado) {
+  $this->usado = $usado;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 ?>
