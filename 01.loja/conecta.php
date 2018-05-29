a1e2d2939d930b0b6a3d3fd27b941edf4822fc4d
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-29 18:09:03 -0300

    Conectando ao banco de dados usando PDO- o mesmo apresenta erros quanto ao login e senha.

--- 01.loja/conecta.php
+++ 01.loja/conecta.php
@@ -1,7 +1,14 @@
 <?php
-$conexao = mysqli_connect("localhost", "root", "", "Produtos");
+
+$conn = new PDO(
+    'mysql:host=localhost;dbname=Produtos', 'root', ''
+);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+/*$conexao = mysqli_connect("localhost", "root", "", "Produtos");
 
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-}
+}*/
