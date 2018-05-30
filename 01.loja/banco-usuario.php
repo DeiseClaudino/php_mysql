1829b7f087822452b476d852f04e39dfee0f2a26
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-30 15:08:11 -0300

    Uncaught Error: Call to a member function quote() on string in /home/deise.pereira/Repositorios/php_mysql/01.loja/banco-usuario.php:7

--- 01.loja/banco-usuario.php
+++ 01.loja/banco-usuario.php
@@ -4,13 +4,13 @@ function buscaUsusario($conexao, $email, $senha)
 {
     $senhaMd5 = md5($senha);
 
-
+    $email = $email->quote($conexao);
     $query ="select * from usuarios where email = '{$email}' and senha = '{$senhaMd5}'";
 
-    $email = $pdo->quote($conexao, $email);
+
 
     $resultado = $pdo->query($conexao, $query);
-    $usuario = $$resultado->fetchAll();
+    $usuario = $resultado->fetchAll();
   //  $email = mysqli_real_escape_string($conexao, $email);
     //$resultado = mysqli_query($conexao, $query);
     //$usuario = mysqli_fetch_assoc($resultado);
