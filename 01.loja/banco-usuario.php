89739b892f0a48b5f3e4342200e551a778c66b59
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-30 16:06:40 -0300

    - Uncaught Error: Call to a member function fetchAll() on boolean in /home/deise.pereira/Repositorios/php_mysql/01.loja/banco-usuario.php:9

--- 01.loja/banco-usuario.php
+++ 01.loja/banco-usuario.php
@@ -5,11 +5,7 @@ function buscaUsusario($conexao, $email, $senha)
     $senhaMd5 = md5($senha);
     $email = $conexao->quote($email);
     $query ="select * from usuarios where email = '{$email}' and senha = '{$senhaMd5}'";
-
-    var_dump($query);
-    die;
-
-    $resultado = $pdo->query($conexao, $query);
+    $resultado = $conexao->query($query);
     $usuario = $resultado->fetchAll();
     //$email = mysqli_real_escape_string($conexao, $email);
     //$resultado = mysqli_query($conexao, $query);
