fdd891278cf84a240874d63da81238878a6b22df
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-30 16:42:09 -0300

    Erros corrigidos, porém login não foi concluido

--- 01.loja/banco-usuario.php
+++ 01.loja/banco-usuario.php
@@ -5,11 +5,10 @@ function buscaUsusario($conexao, $email, $senha)
     $senhaMd5 = md5($senha);
     $email = $conexao->quote($email);
     $query ="select * from usuarios where email = '{$email}' and senha = '{$senhaMd5}'";
-    $resultado = $conexao->query($query);
-    $usuario = $resultado->fetchAll();
+    $resultado = $conexao->query($query, PDO::FETCH_ASSOC);
     //$email = mysqli_real_escape_string($conexao, $email);
     //$resultado = mysqli_query($conexao, $query);
     //$usuario = mysqli_fetch_assoc($resultado);
 
-    return $usuario;
+    return $resultado;
 }
