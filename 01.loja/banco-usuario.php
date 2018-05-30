e7f3ead374edd6dc8b5a4feeeb29d63cd1c57c43
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-30 18:51:04 -0300

    erro no comando  = ->query(, PDO::FETCH_ASSOC); não permite login

--- 01.loja/banco-usuario.php
+++ 01.loja/banco-usuario.php
@@ -4,8 +4,11 @@ function buscaUsusario($conexao, $email, $senha)
 {
     $senhaMd5 = md5($senha);
     $email = $conexao->quote($email);
+    var_dump($email);
     $query ="select * from usuarios where email = '{$email}' and senha = '{$senhaMd5}'";
+    var_dump($query);
     $resultado = $conexao->query($query, PDO::FETCH_ASSOC);
+    var_dump($resultado); die;
     //$email = mysqli_real_escape_string($conexao, $email);
     //$resultado = mysqli_query($conexao, $query);
     //$usuario = mysqli_fetch_assoc($resultado);
