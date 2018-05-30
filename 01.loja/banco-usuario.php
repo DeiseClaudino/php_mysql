c79a0cf2c3f619b14e5854047b000754bf730354
Deise Claudino Almeida Pereira <deise.claudino@gmail.com>
2018-05-30 15:18:04 -0300

    Identando código

--- 01.loja/banco-usuario.php
+++ 01.loja/banco-usuario.php
@@ -3,17 +3,17 @@ require_once("conecta.php");
 function buscaUsusario($conexao, $email, $senha)
 {
     $senhaMd5 = md5($senha);
-
     $email = $email->quote($conexao);
     $query ="select * from usuarios where email = '{$email}' and senha = '{$senhaMd5}'";
 
-
+    var_dump($query);
+    die;
 
     $resultado = $pdo->query($conexao, $query);
     $usuario = $resultado->fetchAll();
-  //  $email = mysqli_real_escape_string($conexao, $email);
+    //$email = mysqli_real_escape_string($conexao, $email);
     //$resultado = mysqli_query($conexao, $query);
     //$usuario = mysqli_fetch_assoc($resultado);
 
-  return $usuario;
+    return $usuario;
 }
--- 01.loja/conecta.php
+++ 01.loja/conecta.php
@@ -2,7 +2,7 @@
 
 $conexao=new PDO("mysql:host=localhost;dbname=Produtos", "root", "");
 
-
+  //var_dump($conexao);die;
 /*$conexao = mysqli_connect("localhost", "root", "", "Produtos");
 
 if (mysqli_connect_errno()) {
